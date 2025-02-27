e0b29dbf4591dea95461a9063fa12479ce71875f
Arman657 <tallycom25@gmail.com>
2025-02-27 17:21:58 +0530

    Fixed bug in usen

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server="http://localhost:8000/api/v2";
+export const server="https://clotree-backendd.vercel.app/api/v2";
 export const backend_url = "https://clotree-backendd.vercel.app/"
